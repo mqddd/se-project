c6ffe9cffc800ea7d39dd2f7d5d7180733ff00ee
mqddd <meghdadd78@gmail.com>
2021-04-16 15:37:11 +0430

    primary models created

--- groupChallenge/models.py
+++ groupChallenge/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=64, null=False, blank=False)
+    last_name = models.CharField(max_length=64)
+    email = models.CharField(max_length=64, null=False, blank=False)
+    password = models.CharField(max_length=64, null=False, blank=False)
+    avatar = models.FileField(upload_to='files/user_avatar')
+
+
+class Challenge(models.Model):
+    title = models.CharField(max_length=128, null=False, blank=False)
+    start_date = models.DateTimeField(null=False, blank=False)
+    end_date = models.DateTimeField(null=False, blank=False)
+    category = models.OneToOneField('Category')
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=64, null=False, blank=False)
+    description = models.CharField(max_length=1024)
+
+
+class Feedback(models.Model):
+    title = models.CharField(max_length=128, null=False, blank=False)
+    content = models.CharField(max_length=1024, null=False, blank=False)
+    owner = models.OneToOneField(User, null=False, blank=False)
+
+
