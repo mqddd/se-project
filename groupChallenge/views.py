152f8934401cdb1e3332ce97e325972901eae321
mqddd <meghdadd78@gmail.com>
2021-05-02 01:16:34 +0430

    get all challenges api created

--- groupChallenge/views.py
+++ groupChallenge/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import *
 
-# Create your views here.
+
+class AllChallengesApiView(APIView):
+
+    def get(self, request, format=None):
+        try:
+            challenges = Challenge.objects.all()
+            data = []
+
+            for challenge in challenges:
+                data.append(
+                    {
+                        "title": challenge.title,
+                        "desc": challenge.description,
+                    }
+                )
+
+            return Response({'data': data}, status=status.HTTP_200_OK)
+        except:
+            return Response({'error': 'error occurred'}, status=status.HTTP_400_BAD_REQUEST)
