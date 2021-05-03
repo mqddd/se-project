acb5b333e1025dca5818c2c2ffb7b304164fa626
m.dehghan <meghdadd78@gmail.com>
2021-05-03 15:33:23 +0430

    create challenge url added

--- groupChallenge/urls.py
+++ groupChallenge/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^challenges/$', views.AllChallengesApiView.as_view(), name='challenges'),
+    url(r'^createchallenge/$', views.CreateChallengeApiView.as_view(), name='createChallenge'),
 ]
